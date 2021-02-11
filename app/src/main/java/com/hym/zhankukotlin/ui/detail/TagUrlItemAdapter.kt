8deaa6a3ea00e51327f9be67328bce1468fed043
hehua2008 <hegan2010@gmail.com>
2021-02-11 19:06:26 +0800

    Create Button View and ImageView through code

--- app/src/main/java/com/hym/zhankukotlin/ui/detail/TagUrlItemAdapter.kt
+++ app/src/main/java/com/hym/zhankukotlin/ui/detail/TagUrlItemAdapter.kt
@@ -2,9 +2,7 @@ package com.hym.zhankukotlin.ui.detail
 
 import android.content.Intent
 import com.google.android.material.button.MaterialButton
-import com.hym.zhankukotlin.databinding.ButtonItemBinding
 import com.hym.zhankukotlin.network.CatagoryItem
-import com.hym.zhankukotlin.ui.BindingViewHolder
 import com.hym.zhankukotlin.ui.NameValueAdapter
 import com.hym.zhankukotlin.ui.main.PreviewItemFragment
 import com.hym.zhankukotlin.ui.tag.TagActivity
@@ -12,20 +10,16 @@ import com.hym.zhankukotlin.util.ViewUtils.getActivityContext
 import java.util.*
 
 class TagUrlItemAdapter : NameValueAdapter<String, String>() {
-    override fun getOnCheckedChangeListener(
-            holder: BindingViewHolder<ButtonItemBinding>, position: Int
-    ): MaterialButton.OnCheckedChangeListener? {
+    override fun getOnCheckedChangeListener(holder: ViewHolder, position: Int)
+            : MaterialButton.OnCheckedChangeListener? {
         return null
     }
 
-    override fun onBindViewHolder(
-            holder: BindingViewHolder<ButtonItemBinding>, position: Int
-    ) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
-        val binding = holder.binding
-        binding.buttonView.isCheckable = false
-        binding.buttonView.text = mNameValues[position].key
-        binding.buttonView.setOnClickListener { v ->
+        holder.button.isCheckable = false
+        holder.button.text = mNameValues[position].key
+        holder.button.setOnClickListener { v ->
             val context = v.getActivityContext() ?: return@setOnClickListener
             val tagUrl = mNameValues[position].value
             val intent = Intent(context, TagActivity::class.java)
@@ -36,9 +30,9 @@ class TagUrlItemAdapter : NameValueAdapter<String, String>() {
         }
     }
 
-    override fun onViewRecycled(holder: BindingViewHolder<ButtonItemBinding>) {
+    override fun onViewRecycled(holder: ViewHolder) {
         super.onViewRecycled(holder)
-        holder.binding.buttonView.setOnClickListener(null)
+        holder.button.setOnClickListener(null)
     }
 
     fun setTagItems(tagItems: List<CatagoryItem>) {
--- app/src/main/java/com/hym/zhankukotlin/ui/main/CatagoryItemAdapter.kt
+++ app/src/main/java/com/hym/zhankukotlin/ui/main/CatagoryItemAdapter.kt
@@ -1,17 +1,14 @@
 package com.hym.zhankukotlin.ui.main
 
 import com.google.android.material.button.MaterialButton
-import com.hym.zhankukotlin.databinding.ButtonItemBinding
 import com.hym.zhankukotlin.network.CatagoryItem
-import com.hym.zhankukotlin.ui.BindingViewHolder
 import com.hym.zhankukotlin.ui.NameValueAdapter
 import java.util.*
 
 class CatagoryItemAdapter(private val mPageViewModel: PageViewModel) :
         NameValueAdapter<String, String>() {
-    override fun getOnCheckedChangeListener(
-            holder: BindingViewHolder<ButtonItemBinding>, position: Int
-    ): MaterialButton.OnCheckedChangeListener {
+    override fun getOnCheckedChangeListener(holder: ViewHolder, position: Int)
+            : MaterialButton.OnCheckedChangeListener {
         return MaterialButton.OnCheckedChangeListener { button, isChecked ->
             if (isChecked) {
                 mPageViewModel.setSubcat(mNameValues[position].value)
@@ -19,10 +16,9 @@ class CatagoryItemAdapter(private val mPageViewModel: PageViewModel) :
         }
     }
 
-    override fun onBindViewHolder(holder: BindingViewHolder<ButtonItemBinding>, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         super.onBindViewHolder(holder, position)
-        val binding = holder.binding
-        binding.buttonView.text = mNameValues[position].key
+        holder.button.text = mNameValues[position].key
     }
 
     fun setTitleSubcatMap(parentCatagoryItem: CatagoryItem) {
