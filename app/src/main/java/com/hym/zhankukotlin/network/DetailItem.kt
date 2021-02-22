hehua2008/ZhanKuMultiplatform

Initial Project

app/src/main/java/com/hym/zhankukotlin/network/DetailItem.kt
@@ -1,0 +1,100 @@
+package com.hym.zhankukotlin.network
+
+class DetailItem private constructor() {
+    @JvmField
+    var title: String? = null
+
+    @JvmField
+    var time: String? = null
+
+    @JvmField
+    var views: String? = null
+
+    @JvmField
+    var comments: String? = null
+
+    @JvmField
+    var favorites: String? = null
+
+    lateinit var categorys: List<CategoryItem>
+
+    lateinit var imgUrls: List<String>
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append(title).append('\n')
+            .append(time).append('\n')
+            .append(views).append('\n')
+            .append(comments).append('\n')
+            .append(favorites).append('\n')
+            .append(categorys).append('\n')
+            .append(imgUrls).append('\n')
+        return sb.toString()
+    }
+
+    class Builder {
+        private var title: String? = null
+        private var time: String? = null
+        private var views: String? = null
+        private var comments: String? = null
+        private var favorites: String? = null
+        private var categorys: MutableList<CategoryItem> = mutableListOf()
+        private var imgUrls: MutableList<String> = mutableListOf()
+
+        fun title(title: String?): Builder {
+            this.title = title
+            return this
+        }
+
+        fun time(time: String?): Builder {
+            this.time = time
+            return this
+        }
+
+        fun views(views: String?): Builder {
+            this.views = views
+            return this
+        }
+
+        fun comments(comments: String?): Builder {
+            this.comments = comments
+            return this
+        }
+
+        fun favorites(favorites: String?): Builder {
+            this.favorites = favorites
+            return this
+        }
+
+        fun addCategory(category: CategoryItem): Builder {
+            categorys.add(category)
+            return this
+        }
+
+        fun addImageUrl(imgUrl: String): Builder {
+            imgUrls.add(imgUrl)
+            return this
+        }
+
+        fun build(): DetailItem {
+            val ret = DetailItem()
+            ret.title = title
+            ret.time = time
+            ret.views = views
+            ret.comments = comments
+            ret.favorites = favorites
+            ret.categorys = categorys
+            ret.imgUrls = imgUrls
+
+            title = null
+            time = null
+            views = null
+            comments = null
+            favorites = null
+            categorys = mutableListOf()
+            imgUrls = mutableListOf()
+
+            return ret
+        }
+    }
+}

app/src/main/java/com/hym/zhankukotlin/network/PreviewResult.kt
@@ -1,0 +1,5 @@
+package com.hym.zhankukotlin.network
+
+class PreviewResult constructor(
+    val previewItems: List<PreviewItem>, val categoryItem: CategoryItem, val pagedArr: IntArray
+)

app/src/main/java/com/hym/zhankukotlin/ui/main/Main2Activity.kt
@@ -1,0 +1,111 @@
+package com.hym.zhankukotlin.ui.main
+
+import android.content.res.ColorStateList
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.view.ViewConfiguration
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import com.hym.zhankukotlin.MyApplication
+import com.hym.zhankukotlin.R
+import com.hym.zhankukotlin.databinding.ActivityMain2Binding
+
+class Main2Activity : AppCompatActivity(), TabConfigurationStrategy {
+    private val mTitles: MutableList<String> = mutableListOf()
+
+    private lateinit var mMainHandler: Handler
+    private lateinit var mBinding: ActivityMain2Binding
+    private lateinit var mSectionsPagerViewModel: SectionsPagerViewModel
+    private lateinit var mMediator: TabLayoutMediator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mMainHandler = Handler()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
+        val sectionsPagerAdapter = SectionsPager2Adapter(this)
+        mBinding.viewPager.adapter = sectionsPagerAdapter
+        mBinding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            private var mSelectedPosition = 0
+            private val mResume = Runnable { MyApplication.imageLoader.resume() }
+
+            override fun onPageScrolled(
+                position: Int, positionOffset: Float, positionOffsetPixels: Int
+            ) {
+                if (position != mSelectedPosition) {
+                    // TODO
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                mSelectedPosition = position
+                val tabCount = mBinding.tabs.tabCount
+                if (tabCount == 0) {
+                    return
+                }
+                for (i in 0 until tabCount) {
+                    val tab = mBinding.tabs.getTabAt(i) ?: continue
+                    val tabView = tab.customView as TextView? ?: continue
+                    if (tab.position == position) {
+                        tabView.textSize = 16f
+                        tabView.typeface = Typeface.DEFAULT_BOLD
+                    } else {
+                        tabView.textSize = 16f
+                        tabView.typeface = Typeface.DEFAULT
+                    }
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state != ViewPager2.SCROLL_STATE_IDLE) {
+                    mMainHandler.removeCallbacks(mResume)
+                    MyApplication.imageLoader.pause()
+                } else {
+                    mMainHandler.postDelayed(
+                        mResume, ViewConfiguration.getDoubleTapTimeout().toLong()
+                    )
+                }
+            }
+        })
+
+        mMediator = TabLayoutMediator(mBinding.tabs, mBinding.viewPager, this)
+        mSectionsPagerViewModel = ViewModelProvider(this).get(SectionsPagerViewModel::class.java)
+        mSectionsPagerViewModel.categoryItems.observe(this, Observer { categoryItems ->
+            mBinding.viewPager.offscreenPageLimit = categoryItems.size
+            sectionsPagerAdapter.setCategoryItems(categoryItems)
+            mTitles.clear()
+            for (categoryItem in categoryItems) {
+                mTitles.add(categoryItem.title)
+            }
+            mMediator.detach()
+            mMediator.attach()
+        })
+
+        mSectionsPagerViewModel.getCategoryItemsFromNetwork()
+    }
+
+    override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+        if (mTitles.isEmpty()) {
+            return
+        }
+        val states = arrayOfNulls<IntArray>(2)
+        states[0] = intArrayOf(android.R.attr.state_selected)
+        states[1] = IntArray(0)
+        val colors = intArrayOf(R.color.white, R.color.black)
+        val colorStateList = ColorStateList(states, colors)
+        val tabView = TextView(this)
+        tabView.text = mTitles[position]
+        tabView.textSize = 16f
+        tabView.setTextColor(colorStateList)
+        tab.customView = tabView
+    }
+}

app/src/main/java/com/hym/zhankukotlin/ui/main/MainActivity.kt
@@ -1,0 +1,65 @@
+package com.hym.zhankukotlin.ui.main
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.ViewConfiguration
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import com.hym.zhankukotlin.MyApplication
+import com.hym.zhankukotlin.R
+import com.hym.zhankukotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mMainHandler: Handler
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mSectionsPagerViewModel: SectionsPagerViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mMainHandler = Handler()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        mBinding.viewPager.adapter = sectionsPagerAdapter
+        mBinding.tabs.setupWithViewPager(mBinding.viewPager)
+        mBinding.viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            private var mSelectedPosition = 0
+            private val mResume = Runnable { MyApplication.imageLoader.resume() }
+
+            override fun onPageScrolled(
+                position: Int, positionOffset: Float, positionOffsetPixels: Int
+            ) {
+                if (position != mSelectedPosition) {
+                    // TODO
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                mSelectedPosition = position
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state != ViewPager.SCROLL_STATE_IDLE) {
+                    mMainHandler.removeCallbacks(mResume)
+                    MyApplication.imageLoader.pause()
+                } else {
+                    mMainHandler.postDelayed(
+                        mResume, ViewConfiguration.getDoubleTapTimeout().toLong()
+                    )
+                }
+            }
+        })
+
+        mSectionsPagerViewModel = ViewModelProvider(this).get(SectionsPagerViewModel::class.java)
+        mSectionsPagerViewModel.categoryItems.observe(this, Observer { categoryItems ->
+            mBinding.viewPager.offscreenPageLimit = categoryItems.size
+            sectionsPagerAdapter.setCategoryItems(categoryItems)
+        })
+
+        mSectionsPagerViewModel.getCategoryItemsFromNetwork()
+    }
+}

app/src/main/java/com/hym/zhankukotlin/ui/tag/TagActivity.kt
@@ -1,0 +1,37 @@
+package com.hym.zhankukotlin.ui.tag
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.hym.zhankukotlin.R
+import com.hym.zhankukotlin.network.CategoryItem
+import com.hym.zhankukotlin.ui.main.PreviewItemFragment
+
+class TagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setContentView(R.layout.tag_activity)
+
+        val categoryItem: CategoryItem =
+            intent.getParcelableExtra(PreviewItemFragment.CATEGORY_ITEM)!!
+        title = categoryItem.title
+
+        if (savedInstanceState === null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PreviewItemFragment.newInstance(categoryItem))
+                .commitNow()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}

hehua2008/ZhanKuKotlin

app/src/main/java/com/hym/zhankukotlin/network/DetailItem.kt
@@ -1,0 +1,100 @@
+package com.hym.zhankukotlin.network
+
+class DetailItem private constructor() {
+    @JvmField
+    var title: String? = null
+
+    @JvmField
+    var time: String? = null
+
+    @JvmField
+    var views: String? = null
+
+    @JvmField
+    var comments: String? = null
+
+    @JvmField
+    var favorites: String? = null
+
+    lateinit var catagorys: List<CatagoryItem>
+
+    lateinit var imgUrls: List<String>
+
+    override fun toString(): String {
+        val sb = StringBuilder()
+        sb.append(title).append('\n')
+            .append(time).append('\n')
+            .append(views).append('\n')
+            .append(comments).append('\n')
+            .append(favorites).append('\n')
+            .append(catagorys).append('\n')
+            .append(imgUrls).append('\n')
+        return sb.toString()
+    }
+
+    class Builder {
+        private var title: String? = null
+        private var time: String? = null
+        private var views: String? = null
+        private var comments: String? = null
+        private var favorites: String? = null
+        private var catagorys: MutableList<CatagoryItem> = mutableListOf()
+        private var imgUrls: MutableList<String> = mutableListOf()
+
+        fun title(title: String?): Builder {
+            this.title = title
+            return this
+        }
+
+        fun time(time: String?): Builder {
+            this.time = time
+            return this
+        }
+
+        fun views(views: String?): Builder {
+            this.views = views
+            return this
+        }
+
+        fun comments(comments: String?): Builder {
+            this.comments = comments
+            return this
+        }
+
+        fun favorites(favorites: String?): Builder {
+            this.favorites = favorites
+            return this
+        }
+
+        fun addCatagory(catagory: CatagoryItem): Builder {
+            catagorys.add(catagory)
+            return this
+        }
+
+        fun addImageUrl(imgUrl: String): Builder {
+            imgUrls.add(imgUrl)
+            return this
+        }
+
+        fun build(): DetailItem {
+            val ret = DetailItem()
+            ret.title = title
+            ret.time = time
+            ret.views = views
+            ret.comments = comments
+            ret.favorites = favorites
+            ret.catagorys = catagorys
+            ret.imgUrls = imgUrls
+
+            title = null
+            time = null
+            views = null
+            comments = null
+            favorites = null
+            catagorys = mutableListOf()
+            imgUrls = mutableListOf()
+
+            return ret
+        }
+    }
+}

app/src/main/java/com/hym/zhankukotlin/network/PreviewResult.kt
@@ -1,0 +1,5 @@
+package com.hym.zhankukotlin.network
+
+class PreviewResult constructor(
+    val previewItems: List<PreviewItem>, val catagoryItem: CatagoryItem, val pagedArr: IntArray
+)

app/src/main/java/com/hym/zhankukotlin/ui/main/Main2Activity.kt
@@ -1,0 +1,111 @@
+package com.hym.zhankukotlin.ui.main
+
+import android.content.res.ColorStateList
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.Handler
+import android.view.ViewConfiguration
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager2.widget.ViewPager2
+import androidx.viewpager2.widget.ViewPager2.OnPageChangeCallback
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
+import com.google.android.material.tabs.TabLayoutMediator.TabConfigurationStrategy
+import com.hym.zhankukotlin.MyApplication
+import com.hym.zhankukotlin.R
+import com.hym.zhankukotlin.databinding.ActivityMain2Binding
+
+class Main2Activity : AppCompatActivity(), TabConfigurationStrategy {
+    private val mTitles: MutableList<String> = mutableListOf()
+
+    private lateinit var mMainHandler: Handler
+    private lateinit var mBinding: ActivityMain2Binding
+    private lateinit var mSectionsPagerViewModel: SectionsPagerViewModel
+    private lateinit var mMediator: TabLayoutMediator
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mMainHandler = Handler()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main2)
+        val sectionsPagerAdapter = SectionsPager2Adapter(this)
+        mBinding.viewPager.adapter = sectionsPagerAdapter
+        mBinding.viewPager.registerOnPageChangeCallback(object : OnPageChangeCallback() {
+            private var mSelectedPosition = 0
+            private val mResume = Runnable { MyApplication.imageLoader.resume() }
+
+            override fun onPageScrolled(
+                position: Int, positionOffset: Float, positionOffsetPixels: Int
+            ) {
+                if (position != mSelectedPosition) {
+                    // TODO
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                mSelectedPosition = position
+                val tabCount = mBinding.tabs.tabCount
+                if (tabCount == 0) {
+                    return
+                }
+                for (i in 0 until tabCount) {
+                    val tab = mBinding.tabs.getTabAt(i) ?: continue
+                    val tabView = tab.customView as TextView? ?: continue
+                    if (tab.position == position) {
+                        tabView.textSize = 16f
+                        tabView.typeface = Typeface.DEFAULT_BOLD
+                    } else {
+                        tabView.textSize = 16f
+                        tabView.typeface = Typeface.DEFAULT
+                    }
+                }
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state != ViewPager2.SCROLL_STATE_IDLE) {
+                    mMainHandler.removeCallbacks(mResume)
+                    MyApplication.imageLoader.pause()
+                } else {
+                    mMainHandler.postDelayed(
+                        mResume, ViewConfiguration.getDoubleTapTimeout().toLong()
+                    )
+                }
+            }
+        })
+
+        mMediator = TabLayoutMediator(mBinding.tabs, mBinding.viewPager, this)
+        mSectionsPagerViewModel = ViewModelProvider(this).get(SectionsPagerViewModel::class.java)
+        mSectionsPagerViewModel.catagoryItems.observe(this, Observer { catagoryItems ->
+            mBinding.viewPager.offscreenPageLimit = catagoryItems.size
+            sectionsPagerAdapter.setCatagoryItems(catagoryItems)
+            mTitles.clear()
+            for (catagoryItem in catagoryItems) {
+                mTitles.add(catagoryItem.title)
+            }
+            mMediator.detach()
+            mMediator.attach()
+        })
+
+        mSectionsPagerViewModel.getCatagoryItemsFromNetwork()
+    }
+
+    override fun onConfigureTab(tab: TabLayout.Tab, position: Int) {
+        if (mTitles.isEmpty()) {
+            return
+        }
+        val states = arrayOfNulls<IntArray>(2)
+        states[0] = intArrayOf(android.R.attr.state_selected)
+        states[1] = IntArray(0)
+        val colors = intArrayOf(R.color.white, R.color.black)
+        val colorStateList = ColorStateList(states, colors)
+        val tabView = TextView(this)
+        tabView.text = mTitles[position]
+        tabView.textSize = 16f
+        tabView.setTextColor(colorStateList)
+        tab.customView = tabView
+    }
+}

app/src/main/java/com/hym/zhankukotlin/ui/main/MainActivity.kt
@@ -1,0 +1,65 @@
+package com.hym.zhankukotlin.ui.main
+
+import android.os.Bundle
+import android.os.Handler
+import android.view.ViewConfiguration
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewpager.widget.ViewPager
+import androidx.viewpager.widget.ViewPager.OnPageChangeListener
+import com.hym.zhankukotlin.MyApplication
+import com.hym.zhankukotlin.R
+import com.hym.zhankukotlin.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var mMainHandler: Handler
+    private lateinit var mBinding: ActivityMainBinding
+    private lateinit var mSectionsPagerViewModel: SectionsPagerViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        mMainHandler = Handler()
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(supportFragmentManager)
+        mBinding.viewPager.adapter = sectionsPagerAdapter
+        mBinding.tabs.setupWithViewPager(mBinding.viewPager)
+        mBinding.viewPager.addOnPageChangeListener(object : OnPageChangeListener {
+            private var mSelectedPosition = 0
+            private val mResume = Runnable { MyApplication.imageLoader.resume() }
+
+            override fun onPageScrolled(
+                position: Int, positionOffset: Float, positionOffsetPixels: Int
+            ) {
+                if (position != mSelectedPosition) {
+                    // TODO
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                mSelectedPosition = position
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {
+                if (state != ViewPager.SCROLL_STATE_IDLE) {
+                    mMainHandler.removeCallbacks(mResume)
+                    MyApplication.imageLoader.pause()
+                } else {
+                    mMainHandler.postDelayed(
+                        mResume, ViewConfiguration.getDoubleTapTimeout().toLong()
+                    )
+                }
+            }
+        })
+
+        mSectionsPagerViewModel = ViewModelProvider(this).get(SectionsPagerViewModel::class.java)
+        mSectionsPagerViewModel.catagoryItems.observe(this, Observer { catagoryItems ->
+            mBinding.viewPager.offscreenPageLimit = catagoryItems.size
+            sectionsPagerAdapter.setCatagoryItems(catagoryItems)
+        })
+
+        mSectionsPagerViewModel.getCatagoryItemsFromNetwork()
+    }
+}

app/src/main/java/com/hym/zhankukotlin/ui/tag/TagActivity.kt
@@ -1,0 +1,37 @@
+package com.hym.zhankukotlin.ui.tag
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.hym.zhankukotlin.R
+import com.hym.zhankukotlin.network.CatagoryItem
+import com.hym.zhankukotlin.ui.main.PreviewItemFragment
+
+class TagActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val actionBar = supportActionBar
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setContentView(R.layout.tag_activity)
+
+        val catagoryItem: CatagoryItem =
+            intent.getParcelableExtra(PreviewItemFragment.CATAGORY_ITEM)!!
+        title = catagoryItem.title
+
+        if (savedInstanceState === null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, PreviewItemFragment.newInstance(catagoryItem))
+                .commitNow()
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}