hehua2008/ZhanKuMultiplatform

Format the world

app/src/main/java/com/hym/zhankukotlin/network/PreviewResult.kt
@@ -1,5 +1,5 @@
 package com.hym.zhankukotlin.network
 
 class PreviewResult constructor(
-    val previewItems: List<PreviewItem>, val categoryItem: CategoryItem, val pagedArr: IntArray
+        val previewItems: List<PreviewItem>, val categoryItem: CategoryItem, val pagedArr: IntArray
 )

app/src/main/java/com/hym/zhankukotlin/util/PictureUtils.kt
@@ -21,7 +21,6 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 
-
 object PictureUtils {
     private const val TAG = "PictureUtils"
 
@@ -41,8 +40,8 @@ object PictureUtils {
 
             imgUrls.forEach { url ->
                 val futureTarget = GlideApp.with(context)
-                    .download(url)
-                    .submit()
+                        .download(url)
+                        .submit()
 
                 val deferred = async(Dispatchers.IO) {
                     val uri = Uri.parse(url)
@@ -50,7 +49,7 @@ object PictureUtils {
 
                     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
                         var dir = Environment.getExternalStoragePublicDirectory(
-                            Environment.DIRECTORY_PICTURES
+                                Environment.DIRECTORY_PICTURES
                         )
                         if (!dir.isDirectory && !dir.mkdirs()) {
                             Log.e(TAG, "getExternalStoragePublicDirectory failed")
@@ -66,17 +65,17 @@ object PictureUtils {
                             src.copyTo(dst, true)
                             try {
                                 MediaStore.Images.Media.insertImage(
-                                    context.contentResolver, dst.absolutePath, name, null
+                                        context.contentResolver, dst.absolutePath, name, null
                                 )
                             } catch (e: IOException) {
                                 Log.e(TAG, "insert $dst to media library failed", e)
                                 return@async null
                             }
                             context.sendBroadcast(
-                                Intent(
-                                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-                                    Uri.parse(dst.absolutePath)
-                                )
+                                    Intent(
+                                            Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                                            Uri.parse(dst.absolutePath)
+                                    )
                             )
                             return@async dst
                         } catch (e: IOException) {
@@ -102,8 +101,8 @@ object PictureUtils {
                             }
                         }
                         values.put(
-                            MediaStore.Images.Media.RELATIVE_PATH,
-                            Environment.DIRECTORY_PICTURES
+                                MediaStore.Images.Media.RELATIVE_PATH,
+                                Environment.DIRECTORY_PICTURES
                         )
                         val externalContentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
                         val resolver = context.contentResolver
@@ -140,9 +139,9 @@ object PictureUtils {
             awaitAll(*deferreds.toTypedArray())
 
             Toast.makeText(
-                context,
-                "Saved ${deferreds[0].await()?.name} etc. ${deferreds.size} files",
-                Toast.LENGTH_SHORT
+                    context,
+                    "Saved ${deferreds[0].await()?.name} etc. ${deferreds.size} files",
+                    Toast.LENGTH_SHORT
             ).show()
         }
     }

hehua2008/ZhanKuKotlin

app/src/main/java/com/hym/zhankukotlin/network/PreviewResult.kt
@@ -1,5 +1,5 @@
 package com.hym.zhankukotlin.network
 
 class PreviewResult constructor(
-    val previewItems: List<PreviewItem>, val catagoryItem: CatagoryItem, val pagedArr: IntArray
+        val previewItems: List<PreviewItem>, val catagoryItem: CatagoryItem, val pagedArr: IntArray
 )

app/src/main/java/com/hym/zhankukotlin/util/PictureUtils.kt
@@ -16,7 +16,6 @@ import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 
-
 object PictureUtils {
     private const val TAG = "PictureUtils"
 
@@ -36,9 +35,9 @@ object PictureUtils {
 
             imgUrls.forEach { url ->
                 val futureTarget = GlideApp.with(context)
-                    .downloadOnly()
-                    .load(url)
-                    .submit()
+                        .downloadOnly()
+                        .load(url)
+                        .submit()
 
                 val deferred = async(Dispatchers.IO) {
                     val uri = Uri.parse(url)
@@ -46,7 +45,7 @@ object PictureUtils {
 
                     if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
                         var dir = Environment.getExternalStoragePublicDirectory(
-                            Environment.DIRECTORY_PICTURES
+                                Environment.DIRECTORY_PICTURES
                         )
                         if (!dir.isDirectory && !dir.mkdirs()) {
                             Log.e(TAG, "getExternalStoragePublicDirectory failed")
@@ -62,17 +61,17 @@ object PictureUtils {
                             src.copyTo(dst, true)
                             try {
                                 MediaStore.Images.Media.insertImage(
-                                    context.contentResolver, dst.absolutePath, name, null
+                                        context.contentResolver, dst.absolutePath, name, null
                                 )
                             } catch (e: IOException) {
                                 Log.e(TAG, "insert $dst to media library failed", e)
                                 return@async null
                             }
                             context.sendBroadcast(
-                                Intent(
-                                    Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
-                                    Uri.parse(dst.absolutePath)
-                                )
+                                    Intent(
+                                            Intent.ACTION_MEDIA_SCANNER_SCAN_FILE,
+                                            Uri.parse(dst.absolutePath)
+                                    )
                             )
                             return@async dst
                         } catch (e: IOException) {
@@ -98,8 +97,8 @@ object PictureUtils {
                             }
                         }
                         values.put(
-                            MediaStore.Images.Media.RELATIVE_PATH,
-                            Environment.DIRECTORY_PICTURES
+                                MediaStore.Images.Media.RELATIVE_PATH,
+                                Environment.DIRECTORY_PICTURES
                         )
                         val externalContentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
                         val resolver = context.contentResolver
@@ -136,9 +135,9 @@ object PictureUtils {
             awaitAll(*deferreds.toTypedArray())
 
             Toast.makeText(
-                context,
-                "Saved ${deferreds[0].await()?.name} etc. ${deferreds.size} files",
-                Toast.LENGTH_SHORT
+                    context,
+                    "Saved ${deferreds[0].await()?.name} etc. ${deferreds.size} files",
+                    Toast.LENGTH_SHORT
             ).show()
         }
     }