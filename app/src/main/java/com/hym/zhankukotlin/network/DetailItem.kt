hehua2008/ZhanKuMultiplatform

Optimized builder mode

app/src/main/java/com/hym/zhankukotlin/network/DetailItem.kt
@@ -1,25 +1,25 @@
 package com.hym.zhankukotlin.network
 
-class DetailItem private constructor() {
+class DetailItem private constructor(
     @JvmField
-    var title: String? = null
-
-    @JvmField
-    var time: String? = null
+    val title: String? = null,
 
     @JvmField
-    var views: String? = null
+    val time: String? = null,
 
     @JvmField
-    var comments: String? = null
+    val views: String? = null,
 
     @JvmField
-    var favorites: String? = null
+    val comments: String? = null,
 
-    lateinit var categorys: List<CategoryItem>
+    @JvmField
+    val favorites: String? = null,
 
-    lateinit var imgUrls: List<String>
+    val categorys: List<CategoryItem> = emptyList(),
 
+    val imgUrls: List<String> = emptyList()
+) {
     override fun toString(): String {
         val sb = StringBuilder()
         sb.append(title).append('\n')
@@ -77,24 +77,7 @@ class DetailItem private constructor() {
         }
 
         fun build(): DetailItem {
-            val ret = DetailItem()
-            ret.title = title
-            ret.time = time
-            ret.views = views
-            ret.comments = comments
-            ret.favorites = favorites
-            ret.categorys = categorys
-            ret.imgUrls = imgUrls
-
-            title = null
-            time = null
-            views = null
-            comments = null
-            favorites = null
-            categorys = mutableListOf()
-            imgUrls = mutableListOf()
-
-            return ret
+            return DetailItem(title, time, views, comments, favorites, categorys, imgUrls)
         }
     }
 }

app/src/main/java/com/hym/zhankukotlin/network/PreviewItem.kt
@@ -2,34 +2,34 @@ package com.hym.zhankukotlin.network
 
 import java.util.*
 
-class PreviewItem private constructor() {
+class PreviewItem private constructor(
     @JvmField
-    var imageUrl: String? = null
+    val imageUrl: String? = null,
 
     @JvmField
-    var targetUrl: String? = null
+    val targetUrl: String? = null,
 
     @JvmField
-    var title: String? = null
+    val title: String? = null,
 
     @JvmField
-    var views: String? = null
+    val views: String? = null,
 
     @JvmField
-    var comments: String? = null
+    val comments: String? = null,
 
     @JvmField
-    var favorites: String? = null
+    val favorites: String? = null,
 
     @JvmField
-    var author: String? = null
+    val author: String? = null,
 
     @JvmField
-    var time: String? = null
+    val time: String? = null,
 
     @JvmField
-    var category: CategoryItem? = null
-
+    val category: CategoryItem? = null
+) {
     override fun hashCode(): Int {
         return Objects.hash(targetUrl ?: imageUrl)
     }
@@ -117,28 +117,17 @@ class PreviewItem private constructor() {
         }
 
         fun build(): PreviewItem {
-            val ret = PreviewItem()
-            ret.imageUrl = imageUrl
-            ret.targetUrl = targetUrl
-            ret.title = title
-            ret.views = views
-            ret.comments = comments
-            ret.favorites = favorites
-            ret.author = author
-            ret.time = time
-            ret.category = category
-
-            imageUrl = null
-            targetUrl = null
-            title = null
-            views = null
-            comments = null
-            favorites = null
-            author = null
-            time = null
-            category = null
-
-            return ret
+            return PreviewItem(
+                imageUrl,
+                targetUrl,
+                title,
+                views,
+                comments,
+                favorites,
+                author,
+                time,
+                category
+            )
         }
     }
 }

hehua2008/ZhanKuKotlin

app/src/main/java/com/hym/zhankukotlin/network/DetailItem.kt
@@ -1,25 +1,25 @@
 package com.hym.zhankukotlin.network
 
-class DetailItem private constructor() {
+class DetailItem private constructor(
     @JvmField
-    var title: String? = null
-
-    @JvmField
-    var time: String? = null
+    val title: String? = null,
 
     @JvmField
-    var views: String? = null
+    val time: String? = null,
 
     @JvmField
-    var comments: String? = null
+    val views: String? = null,
 
     @JvmField
-    var favorites: String? = null
+    val comments: String? = null,
 
-    lateinit var catagorys: List<CatagoryItem>
+    @JvmField
+    val favorites: String? = null,
 
-    lateinit var imgUrls: List<String>
+    val catagorys: List<CatagoryItem> = emptyList(),
 
+    val imgUrls: List<String> = emptyList()
+) {
     override fun toString(): String {
         val sb = StringBuilder()
         sb.append(title).append('\n')
@@ -77,24 +77,7 @@ class DetailItem private constructor() {
         }
 
         fun build(): DetailItem {
-            val ret = DetailItem()
-            ret.title = title
-            ret.time = time
-            ret.views = views
-            ret.comments = comments
-            ret.favorites = favorites
-            ret.catagorys = catagorys
-            ret.imgUrls = imgUrls
-
-            title = null
-            time = null
-            views = null
-            comments = null
-            favorites = null
-            catagorys = mutableListOf()
-            imgUrls = mutableListOf()
-
-            return ret
+            return DetailItem(title, time, views, comments, favorites, catagorys, imgUrls)
         }
     }
 }

app/src/main/java/com/hym/zhankukotlin/network/PreviewItem.kt
@@ -2,34 +2,34 @@ package com.hym.zhankukotlin.network
 
 import java.util.*
 
-class PreviewItem private constructor() {
+class PreviewItem private constructor(
     @JvmField
-    var imageUrl: String? = null
+    val imageUrl: String? = null,
 
     @JvmField
-    var targetUrl: String? = null
+    val targetUrl: String? = null,
 
     @JvmField
-    var title: String? = null
+    val title: String? = null,
 
     @JvmField
-    var views: String? = null
+    val views: String? = null,
 
     @JvmField
-    var comments: String? = null
+    val comments: String? = null,
 
     @JvmField
-    var favorites: String? = null
+    val favorites: String? = null,
 
     @JvmField
-    var author: String? = null
+    val author: String? = null,
 
     @JvmField
-    var time: String? = null
+    val time: String? = null,
 
     @JvmField
-    var catagory: CatagoryItem? = null
-
+    val catagory: CatagoryItem? = null
+) {
     override fun hashCode(): Int {
         return Objects.hash(targetUrl ?: imageUrl)
     }
@@ -117,28 +117,17 @@ class PreviewItem private constructor() {
         }
 
         fun build(): PreviewItem {
-            val ret = PreviewItem()
-            ret.imageUrl = imageUrl
-            ret.targetUrl = targetUrl
-            ret.title = title
-            ret.views = views
-            ret.comments = comments
-            ret.favorites = favorites
-            ret.author = author
-            ret.time = time
-            ret.catagory = catagory
-
-            imageUrl = null
-            targetUrl = null
-            title = null
-            views = null
-            comments = null
-            favorites = null
-            author = null
-            time = null
-            catagory = null
-
-            return ret
+            return PreviewItem(
+                imageUrl,
+                targetUrl,
+                title,
+                views,
+                comments,
+                favorites,
+                author,
+                time,
+                catagory
+            )
         }
     }
 }