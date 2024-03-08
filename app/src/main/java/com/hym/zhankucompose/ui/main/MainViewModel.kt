3726cfd894f8ac39bbea60ea110201f80e7498cf
hehua2008 <hegan2010@gmail.com>
2024-03-08 11:53:21 +0800

    Rename ZhanKuKotlin to ZhanKuCompose

---
+++ app/src/main/java/com/hym/zhankucompose/ui/main/MainViewModel.kt
@@ -0,0 +1,16 @@
+package com.hym.zhankucompose.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    private val _word = MutableLiveData<String>("")
+    val word: LiveData<String> = _word
+
+    fun setSearchWord(word: String) {
+        val word = word.trim()
+        if (_word.value == word) return
+        _word.value = word
+    }
+}
